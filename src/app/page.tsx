ca9665689c2e5d96275392b3b49ab5b32e0b8418
Igorttdp <igorttdp.dev@gmail.com>
2024-10-10 14:14:12 -0300

    feat: prefetch data and passes as props for nested components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import { Subscriptions } from "@/app/components/subscriptions";
 import SubscriptionsReport from "./components/subscriptionsReport";
+import { localApi } from "@/services/api";
+import { ApiResponse } from "@/types/apiResponse";
+import { SubscriptionContent } from "@/services/subscriptions";
 
-export default function Home() {
+export default async function Home() {
+  const {
+    data: { content },
+  } = await localApi.get<ApiResponse<SubscriptionContent>>("/subscriptions");
+
   return (
     <div className="max-w-[1144px] w-full m-auto py-[95px]">
       <Subscriptions />
-      <SubscriptionsReport />
+      <SubscriptionsReport data={content} />
     </div>
   );
 }
