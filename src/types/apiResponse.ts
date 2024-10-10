20526b7ecbd0a97f984317fc69e647c18fe97b35
Igorttdp <igorttdp.dev@gmail.com>
2024-10-10 10:51:00 -0300

    feat: add API Response interfaces and types

---
+++ src/types/apiResponse.ts
@@ -0,0 +1,34 @@
+interface Pageable {
+  sort: Sort[];
+  offset: number;
+  pageNumber: number;
+  pageSize: number;
+  paged: boolean;
+  unpaged: boolean;
+}
+
+interface Sort {
+  direction: string;
+  property: string;
+  ignoreCase: boolean;
+  nullHandling: string;
+  ascending: boolean;
+  descending: boolean;
+}
+
+export type Content<T> = {};
+
+export type ApiResponse<T> = {
+  pageable: Pageable;
+  sort: Sort[];
+  last: boolean;
+  totalPages: number;
+  totalElements: number;
+  size: number;
+  number: number;
+  first: boolean;
+  numberOfElements: number;
+  empty: boolean;
+
+  content: Content<T>[];
+}
