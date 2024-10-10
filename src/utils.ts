4f7730bd0a5e69fc48b172b09a891692f71bab4f
Igorttdp <igorttdp.dev@gmail.com>
2024-10-10 10:49:37 -0300

    feat: add utils for style 'TextField' MUI component

---
+++ src/utils.ts
@@ -0,0 +1,14 @@
+export const textFieldSx = {
+  ".MuiOutlinedInput-notchedOutline": {
+    borderColor: "#CCCCCC"
+  },
+  ".MuiInputBase-input": {
+    padding: "12px 16px",
+  },
+  ".MuiInputLabel-root": {
+    color: "#CCCCCC",
+  },
+  ".MuiInputLabel-root:not(.Mui-focused,.MuiFormLabel-filled)": {
+    top: "-3px",
+  },
+};
