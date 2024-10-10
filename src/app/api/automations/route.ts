522b8bde9a8b3fc65d0515cea50d920bb97fde06
Igorttdp <igorttdp.dev@gmail.com>
2024-10-10 15:49:15 -0300

    feat: add 'automations' get route

---
+++ src/app/api/automations/route.ts
@@ -0,0 +1,17 @@
+import { AxiosError } from "axios";
+import { NextResponse } from "next/server";
+
+import { getAutomations } from "@/services/automations";
+
+export async function GET() {
+  try {
+    const res = await getAutomations();
+    return Response.json(res);
+  } catch (err) {
+    const error = err as AxiosError;
+    return NextResponse.json(
+      { ...error },
+      { status: Number(error.code) || 500 }
+    );
+  }
+}
