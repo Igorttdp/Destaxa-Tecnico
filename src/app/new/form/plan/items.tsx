9edc5b273ef03fe283a93d01bbed7034d5be1e44
Igorttdp <igorttdp.dev@gmail.com>
2024-10-10 15:56:33 -0300

    feat: split code to improve performance

---
+++ src/app/new/form/plan/items.tsx
@@ -0,0 +1,90 @@
+import { useMemo } from "react";
+
+import Image from "next/image";
+
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import TextField from "@mui/material/TextField";
+import MenuItem from "@mui/material/MenuItem";
+
+import { textFieldSx } from "@/utils/textFieldStyles";
+
+import ChevronDownIcon from "@/assets/icons/chevron-down.svg";
+import { Plan } from "@/services/plan";
+import Products from "./products";
+
+interface PlanItemsProps {
+  plan: Plan;
+}
+
+const intl = new Intl.NumberFormat("pt-BR", {
+  style: "currency",
+  currency: "BRL",
+});
+
+const PlanItems = ({ plan }: PlanItemsProps) => {
+  const comissions = useMemo(
+    () => plan.products.reduce((acc, cv) => acc + cv.commission, 0),
+    [plan.products]
+  );
+
+  const total = useMemo(
+    () => plan.products.reduce((acc, cv) => acc + cv.final_price, 0),
+    [plan.products]
+  );
+
+  return (
+    <Accordion
+      className="!mt-[23px] !mb-6 !bg-[#F8F8F8] !shadow-none"
+      defaultExpanded
+    >
+      <AccordionSummary
+        className="!border-[#00B9B5] !border !border-solid m-0 !h-[54px] !min-h-0 [&>*]:!m-0 !rounded-t [&>div]:!justify-between [&>div]:!items-center"
+        expandIcon={<Image src={ChevronDownIcon} alt="Seta" />}
+        aria-controls="panel1-content"
+        id="panel1-header"
+      >
+        <span>{plan.plan_name}</span>
+      </AccordionSummary>
+      <AccordionDetails className="!rounded-b !px-4 !pt-[23px]">
+        <div className="flex gap-[14.84px] items-center">
+          <TextField
+            label="Selecionar funcionalidade"
+            className="w-[509.08px]"
+            select
+            sx={textFieldSx}
+            defaultValue={"f"}
+          >
+            <MenuItem value="f">Funcionalidade</MenuItem>
+          </TextField>
+
+          <button className="bg-transparent border border-[#00B9B5] text-[#00B9B5] hover:bg-[#00B9B5] hover:text-white transition-colors leading-[22.4px] font-bold w-[131px] px-[27.5px] py[10px] rounded h-[42px]">
+            Adicionar
+          </button>
+        </div>
+
+        <Products data={plan.products} />
+
+        <div className="flex justify-end gap-6 mt-6">
+          <div className="flex items-center gap-2">
+            <span className="text-xs leading-[14.52px]">Comissão</span>
+            <span className="font-bold leading-[22.4px] text-[#691F74]">
+              {intl.format(comissions)}
+            </span>
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="text-xs leading-[14.52px]">
+              Total da assinatura
+            </span>
+            <span className="font-bold leading-[22.4px] text-[#691F74]">
+              {intl.format(total)}
+            </span>
+          </div>
+        </div>
+      </AccordionDetails>
+    </Accordion>
+  );
+};
+
+export default PlanItems;
--- src/app/new/form/plan/products.tsx
+++ src/app/new/form/plan/products.tsx
@@ -1,33 +1,51 @@
+"use client";
+
 import TrashIcon from "@/assets/icons/trash.svg";
+import { Product } from "@/services/plan";
 import Image from "next/image";
 
-const Products = () => {
+interface ProductsProps {
+  data: Product[];
+}
+
+const intl = new Intl.NumberFormat("pt-BR", {
+  style: "currency",
+  currency: "BRL",
+});
+
+const Products = ({ data }: ProductsProps) => {
   return (
     <ul className="flex flex-col gap-4 mt-[29.94px]">
-      <li className="flex gap-[27px]">
-        <div className="w-[608px] pt-[15px] pr-[7px] pb-3 pl-5 flex items-center justify-between border border-[#00B9B533] rounded">
-          <div className="flex gap-[27px] items-end">
-            <div className="flex flex-col">
-              <span className="text-[8px] leading-[9.68px]">TEF Padrão</span>
-              <h3 className="text-base leading-[22.4px] font-bold">
-                TEF Padrão
-              </h3>
+      {data.map((product, index) => (
+        <li key={product.plan_product_name + index} className="flex gap-[27px]">
+          <div className="w-[608px] pt-[15px] pr-[7px] pb-3 pl-5 flex items-center justify-between border border-[#00B9B533] rounded">
+            <div className="flex gap-[27px] items-end">
+              <div className="flex flex-col">
+                <span className="text-[8px] leading-[9.68px]">
+                  {product.plan_product_name}
+                </span>
+                <h3 className="text-base leading-[22.4px] font-bold">
+                  {product.plan_product_name}
+                </h3>
+              </div>
+              <div className="bg-[#00B9B51A] text-[#00B9B5] text-[8px] leading-[9.68px] px-2 py-1 rounded-[30px]">
+                Ciclo 1
+              </div>
             </div>
-            <div className="bg-[#00B9B51A] text-[#00B9B5] text-[8px] leading-[9.68px] px-2 py-1 rounded-[30px]">
-              Ciclo 1
+
+            <div className="flex flex-col items-end">
+              <span className="text-[8px] leading-[9.68px] text-[#00B9B5]">
+                Total
+              </span>
+              <span className="leading-[22.4px] font-bold">
+                {intl.format(product.final_price)}
+              </span>
             </div>
           </div>
 
-          <div className="flex flex-col items-end">
-            <span className="text-[8px] leading-[9.68px] text-[#00B9B5]">
-              Total
-            </span>
-            <span className="leading-[22.4px] font-bold">R$ 30,70</span>
-          </div>
-        </div>
-
-        <Image src={TrashIcon} alt="Excluir" />
-      </li>
+          <Image src={TrashIcon} alt="Excluir" />
+        </li>
+      ))}
     </ul>
   );
 };
