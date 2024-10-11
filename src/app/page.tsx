36444163ec169ef6b573c684e3f656cf58d7c3ab
Igorttdp <igorttdp.dev@gmail.com>
2024-10-10 23:21:02 -0300

    feat: move fetch fn to a hook file to improve code legibility and handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
 import { Subscriptions } from "@/app/components/subscriptions";
 import SubscriptionsReport from "./components/subscriptionsReport";
-import { localApi } from "@/services/api";
-import { ApiResponse } from "@/types/apiResponse";
-import { SubscriptionContent } from "@/services/subscriptions";
+import useHome from "./hook/useHome";
 
 export default async function Home() {
-  const {
-    data: { content },
-  } = await localApi.get<ApiResponse<SubscriptionContent>>("/subscriptions");
+  const { content } = await useHome();
 
   return (
     <div className="max-w-[1144px] w-full m-auto py-[95px]">
